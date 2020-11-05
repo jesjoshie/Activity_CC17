3185d99ec62546aacf7ef3b73e0ef29e610250ea
Joshie <kalilinux19701375@gmail.com>
2020-11-05 19:15:04 +0800

    new update in  inside activity

--- app/src/main/java/com/example/activitycc17/MainActivity.kt
+++ app/src/main/java/com/example/activitycc17/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.activitycc17
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.loginBtn).setOnClickListener{login()}
+        findViewById<Button>(R.id.signupBtn).setOnClickListener{signup()}
+    }
+    private fun login(){
+        val intent = Intent(this,insideActivity::class.java)
+        startActivity(intent)
+    }
+    private  fun signup(){
+        val intent1 = Intent(this,Signup::class.java)
+        startActivity(intent1)
+
     }
 }
--- app/src/main/java/com/example/activitycc17/Signup.kt
+++ app/src/main/java/com/example/activitycc17/Signup.kt
@@ -1,11 +1,19 @@
 package com.example.activitycc17
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class Signup : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+        findViewById<Button>(R.id.createBtn).setOnClickListener{create()}
     }
-}
+    private fun create(){
+        val intent = Intent(this,insideActivity::class.java)
+        startActivity(intent)
+    }
+    }
