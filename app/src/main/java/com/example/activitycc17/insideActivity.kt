3e8038f4fa9f68cbe4481423e7cde4e4719f4a00
Joshie <kalilinux19701375@gmail.com>
2020-11-05 18:13:35 +0800

    temp update

---
+++ app/src/main/java/com/example/activitycc17/insideActivity.kt
@@ -0,0 +1,11 @@
+package com.example.activitycc17
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class insideActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inside)
+    }
+}
